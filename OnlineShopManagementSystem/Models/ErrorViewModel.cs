628ee540274daa63dbd3e35d6f030d224091af77
Md Monir Uddin <irfanuddin211@gmail.com>
2020-08-23 10:56:11 +0600

    Add project files.

---
+++ OnlineShopManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineShopManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
