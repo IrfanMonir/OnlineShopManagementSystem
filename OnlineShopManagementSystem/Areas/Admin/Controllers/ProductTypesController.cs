81f20229f72a00d457ae63cd3ecd13a02ce0c538
Md Monir Uddin <irfanuddin211@gmail.com>
2020-08-23 17:50:37 +0600

    Added Product List

---
+++ OnlineShopManagementSystem/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopManagementSystem.Data;
+
+namespace OnlineShopManagementSystem.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+
+        public ProductTypesController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View(_dbContext.ProductTypes.ToList());
+        }
+    }
+}
